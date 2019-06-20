680b18e11272e9c0725b7485e22f0f2dd47c6d39
s20181106117 <1843132025@qq.com>
2019-06-20 11:59:25 +0800

    33

--- judgment03/main.cpp
+++ judgment03/main.cpp
@@ -7,9 +7,34 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <fstream>
+using namespace std;
+int main(int argc, const char * argv[])
+{
+    int a,b;
+    ifstream in1("/Users/s20181106117/Desktop/caipan.txt ");
+    ifstream in2("/Users/s20181106117/Desktop/caipan-2.txt ");
+    ofstream out("/Users/s20181106117/Desktop/chengji.txt ");
+    if(in1.is_open())
+    {
+        cout<<"file ok"<<endl;
+        in1>>a;
+        cout<<a<<" "<<endl;
+        in1.close();
+    }
+    if(in2.is_open())
+    {
+        cout<<"file ok"<<endl;
+        in2>>b;
+        cout<<b<<" "<<endl;
+        in2.close();
+    }
+    if(out.is_open())
+    {
+        out<<"c="<<a+b<<endl;
+        cout<<"c="<<a+b<<endl;
+        out.close();
+    }
+    
     return 0;
 }
